f786e3e514da3e36859c95a29014c4eb5ac0b4d8
Patrick Gonzales <patrickgonzalesquintana@gmail.com>
2024-02-29 10:28:34 -0400

    Se implemento la funcion mostrarTotalizador que muestra un mensaje

--- src/totalizador.spec.js
+++ src/totalizador.spec.js
@@ -1,10 +1,16 @@
 import precioNeto from "./totalizador.js";
+import mostrarPrecioNeto from "./mostrarTotalizador.js";
 
 describe("Totalizador", () => {
   it("deberia mostrar el precio neto", () => {
     expect(precioNeto(3, 2)).toEqual(6);
   });
+  
   it("deberia entergar precio neto", () => {
     expect(precioNeto(5, 2)).toEqual(10);
+  });
+
+  it("deberia mostrar el precio neto de 5 items a un precio de 2 debria mostrar 10", () => {
+    expect(mostrarPrecioNeto(5, 2)).toEqual("El precio neto es: 10");
   });
 });
