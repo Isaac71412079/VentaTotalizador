11796b1b99df3594dd8566d92b43c88e427a2b5a
Isaac71412079 <isaac.rivero@ucb.edu.co>
2024-02-29 10:02:31 -0400

    Arreglos funcionales en la interfaz

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,4 @@
-import precioneto from "./totalizador";
+import precioNeto from "./totalizador";
 
 const cantidad = document.querySelector("#cantidaddeitem");
 const precio = document.querySelector("#precioporitem");
@@ -11,5 +11,5 @@ form.addEventListener("submit", (event) => {
   const cantidaditem = Number.parseInt(cantidad.value);
   const precioitem = Number.parseInt(precio.value);
 
-  div.innerHTML = "<p>" + precioneto(cantidaditem, precioitem) + "</p>";
+  div.innerHTML = "<p>" + precioNeto(cantidaditem, precioitem) + "</p>";
 });
